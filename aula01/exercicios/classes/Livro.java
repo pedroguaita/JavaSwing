fdeba517aa62c9b953406f97dfdcf9ed7ab2c738
Pedro Pereira Guaita <pedropguaita@gmail.com>
2025-08-11 09:55:11 -0300

    Create Livro.java

---
+++ aula01/exercicios/classes/Livro.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplicationaula1.ex002;
+
+/**
+ *
+ * @author 2830482411036
+ */
+public class Livro {
+    public String titulo;
+    public String autor;
+    public int ano;
+}
