abf66b4b40ddee7bae765216a5d7109d7be77f5f
Pedro Pereira Guaita <pedropguaita@gmail.com>
2025-08-20 22:07:15 -0300

    Add files via upload

---
+++ projects/Calculadora/Calculadora2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora2;
+
+/**
+ *
+ * @author pedro
+ */
+public class Calculadora2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
