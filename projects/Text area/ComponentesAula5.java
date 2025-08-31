8f071a7bc68feab1089d1a71b78dc91a268405ab
Pedro Pereira Guaita <pedropguaita@gmail.com>
2025-08-31 14:55:02 -0300

    Add files via upload

---
+++ projects/Text area/ComponentesAula5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.componentesaula5;
+
+/**
+ *
+ * @author pedro
+ */
+public class ComponentesAula5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
